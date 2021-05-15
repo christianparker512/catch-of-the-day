697a0d223aabb843a6657dd290560d82d73d304f
Christian Parker <christianparker512@gmail.com>
2021-05-15 17:07:14 -0500

    classname on the form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-    return <p>Hello!</p>
+    return
+        <form className ="store-selector">
+            <h2>Please Enter a Store</h2>
+        </form>
 }
 }
 export default StorePicker
