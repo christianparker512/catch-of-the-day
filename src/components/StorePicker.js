04d5984db071f2b96bf51652b84aa5acd79f2905
Christian Parker <christianparker512@gmail.com>
2021-05-15 17:26:53 -0500

    html with jsx

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -5,9 +5,10 @@ class StorePicker extends React.Component {
     render() {
 
     return (
-
         <form className="store-selector">
         <h2>Please Enter a store</h2>
+        <input type="text" required placeholder = "Store name"/>
+        <button type="submit">visit store</button>
         </form>
 
     )
