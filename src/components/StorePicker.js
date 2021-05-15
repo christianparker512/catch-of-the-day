360b2f287594c5fca502d6f5d810e4f935da64ce
Christian Parker <christianparker512@gmail.com>
2021-05-15 17:12:30 -0500

    adding react to storepicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,10 +3,9 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-    return
-        <form className ="store-selector">
-            <h2>Please Enter a Store</h2>
-        </form>
+    return React.createElement('p', {'hey'},
+        React.createElement ());
+
 }
 }
 export default StorePicker
