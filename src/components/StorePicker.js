b4805599fefa75fac64a36547d57c4023de323ce
Christian Parker <christianparker512@gmail.com>
2021-05-15 17:23:06 -0500

    fragment removed

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -5,12 +5,11 @@ class StorePicker extends React.Component {
     render() {
 
     return (
-        <Fragment>
-        <p>Fish!</p>
+
         <form className="store-selector">
         <h2>Please Enter a store</h2>
         </form>
-       </Fragment>
+
     )
 }
 }
