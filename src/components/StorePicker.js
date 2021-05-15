c2235878086b0c5ab9e345e8cbd7257e4ae7a5df
Christian Parker <christianparker512@gmail.com>
2021-05-15 17:21:23 -0500

    Fragment added

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,8 +3,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-    return <form className="store-selector"></form>
 
+    return (
+        <Fragment>
+        <p>Fish!</p>
+        <form className="store-selector">
+        <h2>Please Enter a store</h2>
+        </form>
+       </Fragment>
+    )
 }
 }
 export default StorePicker
