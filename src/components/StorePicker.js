17a8ebdac8798a3bcd75abf6c9e620fb2c4bcc36
Christian Parker <christianparker512@gmail.com>
2021-05-15 17:13:57 -0500

    classnmae

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,8 +3,7 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-    return React.createElement('p', {'hey'},
-        React.createElement ());
+    return <form className="store-selector"></form>
 
 }
 }
