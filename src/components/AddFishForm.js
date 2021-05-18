992aad82a82c207b8ddad3a82c91e68d307cbc57
Christian Parker <christianparker512@gmail.com>
2021-05-18 13:59:49 -0500

    add fish form initial commit

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+    render() {
+        return (
+            <div className="inventory">;
+                <h2>Fish ON</h2>
+            </div>
+        );
+    }
+}
+
+export default AddFishForm;
