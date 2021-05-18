77f71e20ccb4eb840bab13c3b133c49c3a4d8c4e
Christian Parker <christianparker512@gmail.com>
2021-05-18 09:50:19 -0500

    Stateless Functional components

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return(
         <div className="catch-of-the-day">
             <div className="menu">
-                <Header tagline="Christian parker" age={500} cool={true}/>
+                <Header tagline="Fresh Seafood Market" age={50} cool={true}/>
             </div>
             <Inventory />
             <Order />
