c7b2fcf518f1d4db5573786f2ddd4480df1623cd
Christian Parker <christianparker512@gmail.com>
2021-05-19 09:49:54 -0500

    add fish form with placeholders

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,11 +1,29 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+    createFish = (event) => {
+        //1. stop the form from submitting
+        event.preventDefault();
+        console.log("Making a fish!")
+    }
     render() {
         return (
-            <div className="inventory">;
-                <h2>Fish ON</h2>
-            </div>
+
+                <form className="fish-edit" onSubmit={this.createFish}>
+                    <input name="name" type="text" placeholder="Name"/>
+                    <input name="price" type="text" placeholder="Price"/>
+                    <input name="status" type="text" placeholder="Status"/>
+                    <select name ="status">
+                        <option value="available">Fresh</option>
+                        <option value="unavailable">Sold Out</option>
+
+                    </select>
+                    <textarea name="desc" placeholder="decs"/>
+                    <input name="image" type="text" placeholder="name"/>
+                    <button type="submit">+ Add Fish</button>
+                </form>
+
+
         );
     }
 }
