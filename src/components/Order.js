35e596a56d0f3f2050354b3a4567ed22378229db
Christian Parker <christianparker512@gmail.com>
2021-05-20 09:58:27 -0500

    get order ids to order

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,12 +2,11 @@ import React from "react";
 
 class Order extends React.Component {
     render() {
+        const orderIds = Object.keys(this.props.order);
         return (
         <div className="order-wrap">
             <h2>Order</h2>
-            <ul>
-                <li></li>
-            </ul>
+            {orderIds}
         </div>
     );
     }
